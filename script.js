90f07c7a7a087d4d63854f775dc7e6945dd70307
Gaurav Agaria <gauravagaria046@gmail.com>
2026-04-03 21:38:50 +0530

    pepsi-responsive-landing-page
    A responsive Pepsi-inspired landing page built using HTML and CSS.
    Features a modern hero section, smooth layout, and mobile-friendly design. Ideal for practicing responsive web development and UI design.

--- script.js
+++ script.js
@@ -1,73 +1,18 @@
-// Init
-var $ = jQuery;
-var animationTime = 20,
-    days = 7;
- 
-$(document).ready(function(){
+let $ = document
 
-    // timer arguments: 
-    //   #1 - time of animation in mileseconds, 
-    //   #2 - days to deadline
+function imgSlider (anything) {
+	$.querySelector(".pepsi").src = anything;
+}
 
-    $('#progress-time-fill, #death-group').css({'animation-duration': animationTime+'s'});
+function changeBgColor(color){
+	const sec = $.querySelector(".sec");
+	sec.style.background = color;
 
-    var deadlineAnimation = function () {
-        setTimeout(function(){
-            $('#designer-arm-grop').css({'animation-duration': '1.5s'});
-        },0);
+}
 
-        setTimeout(function(){
-            $('#designer-arm-grop').css({'animation-duration': '1s'});
-        },4000);
-
-        setTimeout(function(){
-            $('#designer-arm-grop').css({'animation-duration': '0.7s'});
-        },8000);
-
-        setTimeout(function(){
-            $('#designer-arm-grop').css({'animation-duration': '0.3s'});
-        },12000);
-
-        setTimeout(function(){
-            $('#designer-arm-grop').css({'animation-duration': '0.2s'});
-        },15000);
-    };
-
-    function timer(totalTime, deadline) {
-        var time = totalTime * 1000;
-        var dayDuration = time / deadline;
-        var actualDay = deadline;
-
-        var timer = setInterval(countTime, dayDuration);
-
-        function countTime() {
-            --actualDay;
-            $('.deadline-days .day').text(actualDay);
-
-            if (actualDay == 0) {
-                clearInterval(timer);
-                $('.deadline-days .day').text(deadline);
-            }
-        }
-    }
-
-    var deadlineText = function () {
-        var $el = $('.deadline-days');
-        var html = '<div class="mask-red"><div class="inner">' + $el.html() + '</div></div><div class="mask-white"><div class="inner">' + $el.html() + '</div></div>';
-        $el.html(html);
-    }
-
-    deadlineText();
-
-    deadlineAnimation();
-    timer(animationTime, days);
-
-    setInterval(function(){
-        timer(animationTime, days);
-        deadlineAnimation();
-
-        console.log('begin interval', animationTime * 1000);
-
-    }, animationTime * 1000);
-
-});
+function menuToggle(){
+	const toggleMenu = $.querySelector(".toggleMenu")
+	const navigation = $.querySelector(".navigation")
+	toggleMenu.classList.toggle('active')
+	navigation.classList.toggle('active')
+}
